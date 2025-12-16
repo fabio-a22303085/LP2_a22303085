5e4c90e4025b264a2cb42270ae1baeb55b51a7ad
Fabio Jorge <a22303085@alunos.ulht.pt>
2025-12-16 00:00:08 +0000

    www

--- src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/GameManager.java
@@ -276,8 +276,8 @@ public class GameManager {
         Player p = allInfoPlayers.get(currentPlayer[atual]);
 
         // --- 2. VALIDAÇÕES DE LINGUAGEM ---
-        if (p.getPrimeiraLinguagem().equals("Assembly") && nrSpaces > 2) return false;
-        if (p.getPrimeiraLinguagem().equals("C") && nrSpaces > 3) return false;
+        if (p.getPrimeiraLinguagem().equals("Assembly") && nrSpaces > 2) {return false;}
+        if (p.getPrimeiraLinguagem().equals("C") && nrSpaces > 3) {return false;}
 
         //Se morreu não se mexe
         if (p.getEstado().equals("Derrotado")) {
