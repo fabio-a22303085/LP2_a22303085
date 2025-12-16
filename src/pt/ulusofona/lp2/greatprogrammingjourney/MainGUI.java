35821d49c08aeb732a2644d2258c8fed7889f2d5
Fabio Jorge <a22303085@alunos.ulht.pt>
2025-12-16 20:25:48 +0000

    www

--- src/pt/ulusofona/lp2/greatprogrammingjourney/MainGUI.java
+++
@@ -1,5 +0,0 @@
-package pt.ulusofona.lp2.greatprogrammingjourney;
-
-import pt.ulusofona.lp2.greatprogrammingjourney.guiSimulator.AppLauncherKt;
-
-public class MainGUI { public static void main(String[] args) { AppLauncherKt.main(); }}
