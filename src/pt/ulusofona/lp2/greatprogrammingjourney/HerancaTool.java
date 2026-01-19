ab4e2edf532aab764713fc987684afd9b1616c0c
Fabio Jorge <a22303085@alunos.ulht.pt>
2026-01-19 11:59:28 +0000

    www

--- src/pt/ulusofona/lp2/greatprogrammingjourney/HerancaTool.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/HerancaTool.java
@@ -4,7 +4,4 @@ public class HerancaTool extends Tool {
     public HerancaTool(int id, String title) {
         super(id, title);
     }
-    // Se quiseres hardcodear, podes ignorar os argumentos no construtor:
-    // public Heranca() { super(0, "Herança"); }
-    // Mas para manter compatibilidade com o teu loadGame, deixamos assim.
 }
